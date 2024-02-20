729007c4ae87931bbf7ecc0a2cb91181ca70aff9
“samariwa” <samuelmariwa@gmail.com>
2024-02-20 21:25:14 +0300

    Testing CI/CD workflow

--- run.py
+++ run.py
@@ -3,4 +3,4 @@ from app import app, admin_views, auth_views, views, crud_functions, error_views
 if __name__ == "__main__":
     debugger = False
 
-    app.run(host='172.232.220.217', port=8000, debug=debugger)
+    app.run(port=8000, debug=debugger)
